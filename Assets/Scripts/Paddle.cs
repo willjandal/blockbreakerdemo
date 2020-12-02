8b9bc6d4376c8ca1ccad2b5dfbc54a05012318b2
JV Andal <44486596+willjandal@users.noreply.github.com>
2020-12-03 01:11:33 +0800

    added math.clamp function

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -6,6 +6,8 @@ public class Paddle : MonoBehaviour
 {
 
     [SerializeField] float screenWidth = 16f;
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +18,11 @@ public class Paddle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Debug.Log(Input.mousePosition.x / Screen.width * screenWidth);
         float mousePos = Input.mousePosition.x / Screen.width * screenWidth;
         
-        Vector2 paddlePos = new Vector2(mousePos, transform.position.y);
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePos, minX, maxX);
         transform.position = paddlePos;
     }
 }
