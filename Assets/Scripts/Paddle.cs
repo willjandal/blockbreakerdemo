26d3fd01c7423e274af97e0303e59cd144d5562c
JV Andal <44486596+willjandal@users.noreply.github.com>
2020-12-03 00:59:22 +0800

    added paddle movement via Mouse input

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+
+    [SerializeField] float screenWidth = 16f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePos = Input.mousePosition.x / Screen.width * screenWidth;
+        
+        Vector2 paddlePos = new Vector2(mousePos, transform.position.y);
+        transform.position = paddlePos;
+    }
+}
